7339cf4088c6ef6c634e46fb915f75e036957a76
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-04-13 01:14:58 -0500

    Create auth.py

---
+++ backend/app/api/endpoints/auth.py
@@ -0,0 +1,69 @@
+pythonfrom datetime import timedelta
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app.api.deps import get_current_user, get_db
+from app.core.config import settings
+from app.core.security import create_access_token, get_password_hash, verify_password
+from app.models.user import User
+from app.schemas.token import Token
+from app.schemas.user import User as UserSchema, UserCreate
+from app.services.user import create_user, get_user_by_email
+
+router = APIRouter()
+
+
+@router.post("/login", response_model=Token)
+def login_access_token(
+    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
+) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = get_user_by_email(db, email=form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    elif not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": create_access_token(
+            subject=str(user.id), expires_delta=access_token_expires
+        ),
+        "token_type": "bearer",
+    }
+
+
+@router.post("/register", response_model=UserSchema)
+def register_user(*, db: Session = Depends(get_db), user_in: UserCreate) -> Any:
+    """
+    Create new user
+    """
+    user = get_user_by_email(db, email=user_in.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="A user with this email already exists",
+        )
+    
+    # Hash the password and create the user
+    user_data = user_in.dict(exclude={"password"})
+    user_data["hashed_password"] = get_password_hash(user_in.password)
+    return create_user(db, user_data)
+
+
+@router.get("/me", response_model=UserSchema)
+def read_users_me(current_user: User = Depends(get_current_user)) -> Any:
+    """
+    Get current user
+    """
+    return current_user
